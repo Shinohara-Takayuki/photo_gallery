015e1afee8293711492f3a30302a46929bd8304f
nesternik <nesternik@gmail.com>
2017-01-15 23:10:43 +0300

    implemented registration process

--- photo_gallery/urls.py
+++ photo_gallery/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url, include
 import photo_gallery.views
 
 urlpatterns = [
+	url(r'^registration/', photo_gallery.views.registration, name='registration'),
 	url(r'^register_user/', photo_gallery.views.register_user, name='register_user'),
+	url(r'^registered/', photo_gallery.views.registered, name='registered'),
 	url(r'^', include('django.contrib.auth.urls'), {'template_name': 'registration/login.html'}),
 ]
--- photo_gallery/views.py
+++ photo_gallery/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def register_user(request):
-	return render(request, 'registration/register_user.html')
-	
 def mystub(request):
 	return HttpResponse("Hello world")
+	
+def registration(request):
+	return render(request, 'registration/registration.html')
+	
+def register_user(request):
+	user = User.objects.create_user(request.POST['username'], 
+									request.POST['email'], 
+									request.POST['password'])
+	user.last_name = request.POST['lastname']
+	user.first_name = request.POST['firstname']
+	user.save()
+	return HttpResponseRedirect(reverse('registered'))
+	
+def registered(request):
+	return render(request, 'registration/registered.html')
+	
