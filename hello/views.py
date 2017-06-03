7b4d5372d81c89cfa08b334f3dd2a95e50239f62
Shinohara Takayuki <taarkh6651@pasco.co.jp>
2017-06-03 09:56:37 +0900

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -8,10 +8,10 @@ from .models import Greeting
 def index(request):
 	#r = requests.get('http://httpbin.org/status/418')
 	#print r.text
-	times = int(os.environ.get('TIMES',3))
-	return HttpResponse('Hello! ' * times)
+	#times = int(os.environ.get('TIMES',3))
+	#return HttpResponse('Hello! ' * times)
 	#return HttpResponse('Hello from Python!')
-	#return render(request, 'index.html')
+	return render(request, 'index.html')
 	
 def db(request):
 
